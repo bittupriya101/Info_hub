90615c8a49373dd6b43edb9bd88e80181cb5782f
Priya Maurya <137216436+bittupriya101@users.noreply.github.com>
2024-09-04 22:25:49 +0530

    Add files via upload

---
+++ News Application/Demo.java
@@ -0,0 +1,27 @@
+// public class Demo {
+//     static {
+//         System.out.println("Hi");
+//     }
+//     {
+//         System.out.println("Bye");
+//     }
+//     {
+//         public static void main(String args[]) {
+//             System.out.println("Hello");
+//         }
+//     }
+// }
+
+public class Demo {
+    static {
+        System.out.println("Hi");
+    }
+
+    {
+        System.out.println("Bye");
+    }
+
+    public static void main(String args[]) {
+        System.out.println("Hello");
+    }
+}
